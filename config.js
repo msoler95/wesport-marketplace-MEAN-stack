bc0372e88a3c6185c7c9f5b51963b56e57f6c34f
Marc <marc.soler.araguas@est.fib.upc.edu>
2017-09-26 13:17:48 +0200

    push

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ exports.jwt_secret = "S0m3ultr4s3cur3s3cr3t";
 
 exports.db_path = 'mongodb://localhost/example_07';
 exports.confirmationUrl = "http://178.62.35.246:8080/user/confirmation" //"wespo.es/user/confirmation";
-exports.mainUrl = "http://178.62.35.246:8080";
+exports.mainUrl = "178.62.35.246:8080";
 
 exports.serverPort = '8080'
 exports.serverUrl = '178.62.35.246'
