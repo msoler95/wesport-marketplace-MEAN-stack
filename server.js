d43223463d4a5aeaf7d44db710c681381a73a6da
Marc <marc.soler.araguas@est.fib.upc.edu>
2017-09-26 13:07:32 +0200

    push

--- server.js
+++ server.js
@@ -38,9 +38,11 @@ app.get('*', function(req, res){
   res.sendfile(__dirname + '/public/index.html');
 });
 
+//Vigilar amb les fotos al fer el deploy
+var port = config.serverPort;	//80 //8080
+var adress = config.serverUrl; //var adress = "178.62.35.246"; //localhost 
 
+http.createServer(app).listen(port, adress, function() {
+	console.log('Conected on local:8080! :D' + new Date() )
+});
 
-
-http.createServer(app, function() {
-	console.log('Conectado!');
-}).listen(8080);
