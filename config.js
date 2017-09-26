9150956c03b158448fa1ecf22717bf26b8b057a0
Marc <marc.soler.araguas@est.fib.upc.edu>
2017-09-26 12:36:36 +0200

    changed gitignore

---
+++ config.js
@@ -0,0 +1,6 @@
+//In production environment, you should use some secure-generated key stored in file system.
+exports.jwt_secret = "S0m3ultr4s3cur3s3cr3t";
+
+exports.db_path = 'mongodb://localhost/example_07';
+exports.confirmationUrl = "wespo.es/user/confirmation";
+exports.mainUrl = "wespo.es";
---
+++ public/angular-services/constants.js
@@ -0,0 +1,9 @@
+angular.module('WeSport')
+ 
+.constant('AUTH_EVENTS', {
+  notAuthenticated: 'auth-not-authenticated'
+})
+ 
+.constant('API_ENDPOINT', {
+  url: '178.62.35.246:80' //'http://178.62.35.246:80'	// 'localhost:8080'
+});
